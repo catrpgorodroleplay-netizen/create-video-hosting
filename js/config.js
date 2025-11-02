80a0f0133c52f71e5fafce65c3dc5794f6bd0fde
catrpgorodroleplay-netizen <catrpgorodroleplay@gmail.com>
2025-11-02 04:40:30 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,19 +1,4 @@
-// Конфигурация с Piped API
 const CONFIG = {
-    // Стабильные Piped instances
-    pipedInstances: [
-        'https://pipedapi.kavin.rocks',
-        'https://pipedapi.moomoo.me', 
-        'https://pipedapi-libre.kavin.rocks',
-        'https://piped-api.garudalinux.org'
-    ],
-    
-    defaultInstance: 'https://pipedapi.kavin.rocks',
-    
-    // Настройки
-    maxResults: 20,
-    
-    // Категории для поиска
     categories: {
         music: 'music',
         gaming: 'gaming',
